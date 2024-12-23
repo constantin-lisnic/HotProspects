be3351fad4a1992852b58dcd652f6b9c25e913c4
Constantin Lisnic <constantin@lisnic.dev>
2024-12-23 13:04:05 +0000

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Constantin Lisnic on 23/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
